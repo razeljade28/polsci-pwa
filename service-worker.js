fbf788049cb90b35ca7e5c09721996743179ff30
razeljade28 <razeljadehijastro28@gmail.com>
2026-07-19 16:24:14 +0800

    back to last

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,39 @@
-const CACHE = 'civic-circle-v1';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+const CACHE_NAME = 'sepolscis-v4';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/signup.html',
+  '/style.css',
+  '/app.js',
+  '/learning-content.js',
+  '/logo.png',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
